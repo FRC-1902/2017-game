b455b6cd1aba9f4d84b9e6e172ea89e466cb419b
Lenovo 1 <weegeemus@gmail.com>
2017-01-23 17:08:10 -0500

    cleanup and final variables

--- src/main/java/com/explodingbacon/steambot/Map.java
+++ src/main/java/com/explodingbacon/steambot/Map.java
@@ -4,17 +4,17 @@ public class Map {
 
     //PWM
 
-    public static int LEFT_DRIVE_1 = 9; //
-    public static int LEFT_DRIVE_2 = 6; //
+    public static final int LEFT_DRIVE_1 = 9;
+    public static final int LEFT_DRIVE_2 = 6;
 
-    public static int RIGHT_DRIVE_1 = 1; //
-    public static int RIGHT_DRIVE_2 = 8; //
+    public static final int RIGHT_DRIVE_1 = 1;
+    public static final int RIGHT_DRIVE_2 = 8;
 
-    public static int STRAFE_DRIVE_1 = 7; //
-    public static int STRAFE_DRIVE_2 = 0; //
+    public static final int STRAFE_DRIVE_1 = 7;
+    public static final int STRAFE_DRIVE_2 = 0;
 
     //Solenoid
 
-    public static int RINGLIGHT = 0;
+    public static final int RINGLIGHT = 0;
 
 }
